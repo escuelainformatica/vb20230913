ac0c08488445d175e72227252d63598a35fbbdaa
jorgecc <jcastro@eftec.cl>
2023-09-13 10:44:53 -0300

    primero

---
+++ Module1.vb
@@ -0,0 +1,35 @@
+﻿Imports System.Linq
+Module Module1
+
+    Sub Main()
+        Dim productos = genereListado()
+        Debug.WriteLine("-- ordenar los productos: --")
+        Dim productosOrdenados = (From prod In productos
+                                  Order By prod.Nombre).ToList()
+        mostrarProductos(productosOrdenados)
+        Debug.WriteLine("-- filtrado por categoria (bebida): --")
+        Dim productosFiltrados = (From prod In productos
+                                  Where prod.Categoria = "bebida").ToList()
+        mostrarProductos(productosFiltrados)
+        Debug.WriteLine("-- filtrado por categoria (bebida) y ademas por precio mayor a 20: --")
+
+        Dim productosFiltrados2 = (From prod In productos
+                                   Where prod.Categoria = "bebida"
+                                   Where prod.Precio > 20).ToList()
+        mostrarProductos(productosFiltrados2)
+    End Sub
+    Sub mostrarProductos(productos As List(Of Producto))
+        productos.ForEach(Sub(producto) Debug.WriteLine($"{producto.Nombre} | {producto.Precio} | {producto.Cantidad} | {producto.Categoria}"))
+    End Sub
+    Function genereListado() As List(Of Producto)
+        Dim productos As New List(Of Producto)
+        productos.Add(New Producto("cocacola", 10, 30, "bebida"))
+        productos.Add(New Producto("fanta", 30, 10, "bebida"))
+        productos.Add(New Producto("sprite", 20, 50, "bebida"))
+        productos.Add(New Producto("samsung", 400, 20, "tecnologia"))
+        productos.Add(New Producto("apple", 300, 5, "tecnologia"))
+        productos.Add(New Producto("microsoft", 500, 2, "tecnologia"))
+        Return productos
+    End Function
+
+End Module
---
+++ Producto.vb
@@ -0,0 +1,17 @@
+﻿Public Class Producto
+    Public Property Nombre As String
+    Public Property Precio As Integer
+    Public Property Cantidad As Integer
+    Public Property Categoria As String
+    Public Sub New()
+
+    End Sub
+
+    ' constructor.
+    Public Sub New(nombre As String, precio As Integer, Optional Cantidad As Integer = 0, Optional Categoria As String = "SIN")
+        Me.Nombre = nombre
+        Me.Precio = precio
+        Me.Cantidad = Cantidad
+        Me.Categoria = Categoria
+    End Sub
+End Class
